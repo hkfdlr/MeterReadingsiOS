a46e902c848cb91f85b76e4c1fb143ff1202310c
Hauke Fiedler <hauke_fiedler@gmx.de>
2021-06-30 14:25:21 +0200

    add infrastructure to repo  

---
+++ MeterReadingsInfrastructure/Sources/MeterReadingsInfrastructure/MeterReadingDatabaseModel.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Student on 09.06.21.
+//
+
+import Foundation
+
+class MeterReadingDatabaseModel {
+    
+    private func MeterReadingDatabaseModel() {
+        
+    }
+    
+}
